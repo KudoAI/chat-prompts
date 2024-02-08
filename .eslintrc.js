8058968b168bb7fff2869ce0844c35dc7cdb9139
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-08 08:54:08 -0800

    Moved JSON `extends` into `overrides`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'extends': ['eslint:recommended', 'plugin:json-schema-validator/recommended'],
+    'extends': ['eslint:recommended'],
     'rules': {
         'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
         'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }], // enforce single quotes for string literals
@@ -11,5 +11,6 @@ module.exports = {
         'no-useless-escape': 'off' // allow all escape chars cause ESLint sucks at detecting truly useless ones
     },
     'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
+    'overrides': [{ 'files': ['**/*.json'], 'extends': ['plugin:json-schema-validator/recommended'] }],
     'env': { 'browser': true, 'node': true, 'es6': true }
 };
