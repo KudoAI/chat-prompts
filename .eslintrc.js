6456b244af7a30b5cf235a68e6cf85011513c50a
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-08 09:00:20 -0800

    Added eslint-plugin-yml dev dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
         'no-useless-escape': 'off' // allow all escape chars cause ESLint sucks at detecting truly useless ones
     },
     'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
-    'overrides': [{ 'files': ['**/*.json'], 'extends': ['plugin:json-schema-validator/recommended'] }],
+    'overrides': [
+        { 'files': ['**/*.json'], 'extends': ['plugin:json-schema-validator/recommended'] },
+        { 'files': ['**/*.yml'], 'extends': ['plugin:yml/standard'] }
+    ],
     'env': { 'browser': true, 'node': true, 'es6': true }
 };
