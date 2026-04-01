9d7816ef53c4db0c087b0f10be535d63b3c678c5
Adam Lui <adam@kudoai.com>
2026-04-01 03:31:22 -0700

    Replaced nightmarishly slow ESLint w/ Py pre-commit hooks

---
+++ noxfile.py
@@ -0,0 +1,11 @@
+import nox
+
+def session(func) : return nox.session(venv_backend='none', name=func.__name__.replace('_', '-'))(func)
+
+@session
+def lint(session): # staged files
+    files = session.run('git', 'diff', '--cached', '--name-only', '--relative', silent=True, log=False).splitlines()
+    if files : session.run('pre-commit', 'run', '--files', *files, *session.posargs)
+@session
+def lint_all(session): # all files
+    session.run('pre-commit', 'run', '--all-files', *session.posargs)
