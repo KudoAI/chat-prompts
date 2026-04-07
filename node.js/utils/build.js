267b938e7142e6550b5e96d291c8d88aaaa03cc4
Adam Lui <adam@kudoai.com>
2026-04-07 04:32:37 -0700

    Added pkg name to build complete log

--- node.js/utils/build.js
+++ node.js/utils/build.js
@@ -4,11 +4,11 @@ const fs = require('fs'),
       file = require('./lib/file'),
      _log = require('./lib/log')
 
-const pkgVer = require('../package.json').version
+const pkg = require('../package.json')
 
 if (fs.existsSync('dist')) fs.rmSync('dist', { recursive: true, force: true })
 fs.mkdirSync('dist', { recursive: true })
 file.copy('src/index.js', 'dist/index.js')
 file.copy('ai-personas.json', 'dist/ai-personas.json')
 
-_log.success(`v${pkgVer} build complete!`)
+_log.success(`${pkg.name} v${pkg.ver} build complete!`)
