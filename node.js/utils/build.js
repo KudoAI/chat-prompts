3ed6a8eef3c060af8699e7c7c443ec34d99f856d
Adam Lui <adam@kudoai.com>
2026-04-15 10:18:45 -0700

    Added build

--- node.js/utils/build.js
+++ node.js/utils/build.js
@@ -10,5 +10,6 @@ if (fs.existsSync('dist')) fs.rmSync('dist', { recursive: true, force: true })
 fs.mkdirSync('dist', { recursive: true })
 file.copy('src/index.js', 'dist/index.js')
 file.copy('ai-personas.json', 'dist/ai-personas.json')
+fs.writeFileSync('dist/ai-personas.min.json', JSON.stringify(JSON.parse(fs.readFileSync('ai-personas.json', 'utf8'))))
 
 _log.success(`${pkg.name} v${pkg.version} build complete!`)
