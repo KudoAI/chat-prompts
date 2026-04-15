2700f2c468e1264adf6f1cdfd674d67cb221843f
Adam Lui <adam@kudoai.com>
2026-04-15 10:28:08 -0700

    Added minify log

--- node.js/utils/build.js
+++ node.js/utils/build.js
@@ -10,6 +10,10 @@ if (fs.existsSync('dist')) fs.rmSync('dist', { recursive: true, force: true })
 fs.mkdirSync('dist', { recursive: true })
 file.copy('src/index.js', 'dist/index.js')
 file.copy('ai-personas.json', 'dist/ai-personas.json')
+console.info(
+     `Minifying ${_log.colors.bo}ai-personas.json${_log.colors.nc} to ${
+         _log.colors.by}dist/ai-personas.min.json${_log.colors.nc}...`
+)
 fs.writeFileSync('dist/ai-personas.min.json', JSON.stringify(JSON.parse(fs.readFileSync('ai-personas.json', 'utf8'))))
 
 _log.success(`${pkg.name} v${pkg.version} build complete!`)
