f69ed0f732907bdcc169accfe8e78cdc3f0a5de3
Adam Lui <adam@kudoai.com>
2026-04-15 00:41:34 -0700

    Deleted line breaks

--- utils/alphabetize-personas.py
+++ utils/alphabetize-personas.py
@@ -6,10 +6,8 @@ personas_path = Path(__file__).parent.parent / 'data/ai-personas.json'
 
 log.info(f'Reading {personas_path}...')
 personas = data.json.read(personas_path)
-
 log.info('Alphabetizing personas...')
 personas = dict(sorted(personas.items(), key = lambda item: item[0].lower()))
-
 log.info(f'Saving {len(personas)} personas...')
 data.json.write(personas_path, personas, style='compact')
 
